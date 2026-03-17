8b76ca391371f2e4268d312655cbf76ac658575b
Danny <amanda.miller43@snhu.edu>
2026-03-17 17:37:39 -0500

    Niagara Particles

--- Source/Miller_GAM415/Miller_GAM415Projectile.h
+++ Source/Miller_GAM415/Miller_GAM415Projectile.h
@@ -8,6 +8,7 @@
 
 class USphereComponent;
 class UProjectileMovementComponent;
+class UNiagaraSystem;
 
 UCLASS(config=Game)
 class AMiller_GAM415Projectile : public AActor
@@ -36,6 +37,9 @@ class AMiller_GAM415Projectile : public AActor
 
 	UPROPERTY()
 	UMaterialInstanceDynamic* dmiMat; //Creates Dynamic Material
+
+	UPROPERTY(EditAnywhere)
+	UNiagaraSystem* colorP;
 
 public:
 	AMiller_GAM415Projectile();
